f23a6183ab8fa8d15ebe38f1bce90a0e204a2668
Travis Reynolds <travis@travisreynolds.dev>
2022-03-01 01:27:04 +0000

    Add demo server config

---
+++ starters/vue/ssg.server.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+import { ServerConfig } from '@travisreynolds/ssg'
+
+const serverConfig: ServerConfig = {
+  data: async ({ store }) => {
+    const posts = Array.from({ length: 1000 }, (_, i) => ({
+      id: i + 1,
+      slug: faker.lorem.slug(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraphs(6),
+      coverImage: faker.image.imageUrl(800, 400, undefined, true, true)
+    }))
+
+    const collection = store.createCollection('post')
+    collection.add(posts)
+  }
+}
+
+export default serverConfig
