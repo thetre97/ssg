b64634e44c95567d387a47b1fb86563bf1af7a4b
Travis Reynolds <travis@travisreynolds.dev>
2022-03-01 02:33:31 +0000

    Update slug word count

--- starters/vue/ssg.server.ts
+++ starters/vue/ssg.server.ts
@@ -5,7 +5,7 @@ const serverConfig: ServerConfig = {
   data: async ({ store }) => {
     const posts = Array.from({ length: 1000 }, (_, i) => ({
       id: i + 1,
-      slug: faker.lorem.slug(),
+      slug: faker.lorem.slug(6),
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraphs(6),
       coverImage: faker.image.imageUrl(800, 400, undefined, true, true)
