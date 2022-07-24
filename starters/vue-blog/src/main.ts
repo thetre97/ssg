54793ac50aae1fe578a0e54d970ceeed38bd78ee
Travis Reynolds <travis@travisreynolds.dev>
2022-07-24 23:39:53 +0100

    rm unused file

--- starters/vue-blog/src/main.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Load this file, and run inside our render hook?
-import 'virtual:windi.css'
-import { App } from 'vue'
-
-// TODO: Turn this into a helper function
-// export default windApp((app) => {})
-export default function client(app: App<Element>) {
-
-}
